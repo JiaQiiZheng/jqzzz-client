dae80b4eb68fc53cb6ebbd9d376dc3680bff66e8
Jiaqi <dxq7nz@virginia.edu>
2023-01-26 02:00:28 -0500

    firstClient

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://jqzzz-server.vercel.app/",
+  baseURL: "https://jqzzz-server.vercel.app/register",
   // baseURL: "http://localhost:8080",
 });
