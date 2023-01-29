333421453e91db25448d6b6ab701d7d29dfd7a5a
Jiaqi <dxq7nz@virginia.edu>
2023-01-29 16:39:51 -0500

    fixLoop

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import homeImg from "../../images/home/Autumn in 2022.jpg";
 
 export default function Home() {
+  // console.log(homeImg);
   return (
     <div className="section_home">
       <img src={homeImg} className="home_img" alt="home_Img" />
